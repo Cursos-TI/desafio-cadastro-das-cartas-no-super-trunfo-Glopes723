5c5d496fece7490871935d396b9cc7e4de494938
Glopes723 <glopes723@gmail.com>
2025-02-21 20:29:50 +0000

    Cadastrei e finaleizei as duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,3 +18,52 @@ int main() {
     int pontos_turisticos2;
     
     printf("Bem-vindo ao Super Trunfo - Países!\n\n");
+
+     // Cadastro da primeira cidade
+     printf("Cadastro da cidade %s\n", codigo1);
+     printf("Nome da cidade: ");
+     scanf(" %[^\n]", nome1); 
+     printf("População: ");
+     scanf("%d", &populacao1);
+     printf("Área: ");
+     scanf("%f", &area1);
+     printf("PIB: ");
+     scanf("%f", &pib1);
+     printf("Número de pontos turísticos: ");
+     scanf("%d", &pontos_turisticos1);
+     printf("\n");
+ 
+     // Cadastro da segunda cidade
+     printf("Cadastro da cidade %s\n", codigo2);
+     printf("Nome da cidade: ");
+     scanf(" %[^\n]", nome2);
+     printf("População: ");
+     scanf("%d", &populacao2);
+     printf("Área: ");
+     scanf("%f", &area2);
+     printf("PIB: ");
+     scanf("%f", &pib2);
+     printf("Número de pontos turísticos: ");
+     scanf("%d", &pontos_turisticos2);
+     printf("\n");
+ 
+     // Exibir a primeira cidade cadastrada
+     printf("Cidade: %s\n", codigo1);
+     printf("Nome: %s\n", nome1);
+     printf("População: %d\n", populacao1);
+     printf("Área: %.2f\n", area1);
+     printf("PIB: %.2f\n", pib1);
+     printf("Número de pontos turísticos: %d\n", pontos_turisticos1);
+     printf("\n");
+ 
+     // Exibir a segunda cidade cadastrada
+     printf("Cidade: %s\n", codigo2);
+     printf("Nome: %s\n", nome2);
+     printf("População: %d\n", populacao2);
+     printf("Área: %.2f\n", area2);
+     printf("PIB: %.2f\n", pib2);
+     printf("Número de pontos turísticos: %d\n", pontos_turisticos2);
+     printf("\n");
+ 
+     return 0;
+ }
